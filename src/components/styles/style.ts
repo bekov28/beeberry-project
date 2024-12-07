4378103382bd0f8b64130176d5bd1f3207cca29e
Bekov28 <orziqulov_berdiyor@mail.ru>
2024-12-07 23:10:11 +0900

    main page fixes

--- src/components/styles/style.ts
+++ src/components/styles/style.ts
@@ -19,7 +19,7 @@ export const NavbarCon = styled.div`
   padding: 20px 50px;
   justify-content: center;
   align-items: center;
-  gap: 200px;
+  gap: 150px;
   background: var(--Black-separate, #201a18);
   z-index: 5;
 `;
